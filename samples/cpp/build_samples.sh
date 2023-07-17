dc8c8eee0947630e9db13cf52943eb7f245cd538
HU Yuan2 <yuan2.hu@intel.com>
2023-07-17 20:30:50 +0800

    fix scripts error of samples build
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- samples/cpp/build_samples.sh
+++ samples/cpp/build_samples.sh
@@ -102,7 +102,7 @@ fi
 mkdir -p "$samples_build_dir"
 cd "$samples_build_dir"
 $CMAKE_EXEC -DCMAKE_BUILD_TYPE=Release "$SAMPLES_SOURCE_DIR"
-$CMAKE_EXEC --build "$samples_build_dir" --config Release -- -j $NUM_THREADS
+$CMAKE_EXEC --build . --config Release -- -j $NUM_THREADS
 
 if [ "$sample_install_dir" != "" ]; then
     $CMAKE_EXEC -DCMAKE_INSTALL_PREFIX="$sample_install_dir" -DCOMPONENT=samples_bin -P cmake_install.cmake
