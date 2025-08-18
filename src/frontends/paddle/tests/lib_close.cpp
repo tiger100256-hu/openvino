c6e89356c45c47b83138e6a81079baacfc03721d
HU Yuan2 <yuan2.hu@intel.com>
2025-08-18 17:12:49 +0800

    fix crash in test
    Signed-off-by: HU Yuan2 <yuan2.hu@intel.com>

--- src/frontends/paddle/tests/lib_close.cpp
+++ src/frontends/paddle/tests/lib_close.cpp
@@ -9,11 +9,18 @@
 
 using namespace testing;
 using namespace ov::util;
+auto get_tensor_name = [] -> const std::string {
+    if (std::string(TEST_ENABLE_PIR) == "1") {
+        return "3";
+    } else {
+        return "conv2d_0.tmp_0";
+    }
+};
 
 INSTANTIATE_TEST_SUITE_P(
     Paddle,
     FrontendLibCloseTest,
     Values(std::make_tuple("paddle",
                            path_join({TEST_PADDLE_MODELS_DIRNAME, "conv2d_relu/conv2d_relu" + std::string(TEST_PADDLE_MODEL_EXT)}).string(),
-                           "conv2d_0.tmp_0")),
+                           get_tensor_name())),
     FrontendLibCloseTest::get_test_case_name);
